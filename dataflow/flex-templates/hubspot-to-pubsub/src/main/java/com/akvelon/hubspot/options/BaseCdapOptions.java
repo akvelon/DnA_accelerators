d4c845760d6e96055e39692c74e12ea0a78e063f
SadyrovZhakhongir <zhakhongir.sadyrov@akvelon.com>
2023-05-15 15:40:24 +0600

    Added hubspot-to-pubsub flex-template

---
+++ dataflow/flex-templates/hubspot-to-pubsub/src/main/java/com/akvelon/hubspot/options/BaseCdapOptions.java
@@ -0,0 +1,30 @@
+package com.akvelon.hubspot.options;
+
+import io.cdap.plugin.common.Constants;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.Validation;
+
+/**
+ * The {@link BaseCdapOptions} interface provides the custom execution options passed by the
+ * executor at the command-line for examples with Cdap plugins.
+ */
+public interface BaseCdapOptions extends PipelineOptions {
+
+    @Validation.Required
+    @Description(Constants.Reference.REFERENCE_NAME_DESCRIPTION)
+    String getReferenceName();
+
+    void setReferenceName(String referenceName);
+
+    @Description(
+            "The Cloud Pub/Sub topic to publish to. "
+                    + "The name should be in the format of "
+                    + "projects/<project-id>/topics/<topic-name>.")
+    @Validation.Required
+    String getOutputTopic();
+
+    void setOutputTopic(String outputTopic);
+
+
+}
---
+++ dataflow/flex-templates/hubspot-to-pubsub/src/main/java/com/akvelon/hubspot/templates/HubspotToPubSub.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2023 Akvelon Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.akvelon.hubspot.templates;
+
+import com.akvelon.hubspot.options.CdapHubspotSourceOptions;
+import com.akvelon.hubspot.transforms.FormatInputTransform;
+import com.akvelon.hubspot.utils.JsonElementCoder;
+import com.akvelon.hubspot.utils.PluginConfigOptionsConverter;
+import com.google.gson.JsonElement;
+import java.util.Map;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.KvCoder;
+import org.apache.beam.sdk.coders.NullableCoder;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.hadoop.WritableCoder;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapValues;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.hadoop.io.NullWritable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * HubspotToPubsub Pipeline.
+ */
+public class HubspotToPubSub {
+    /* Logger for class.*/
+    private static final Logger LOG = LoggerFactory.getLogger(HubspotToPubSub.class);
+
+    /**
+     * Main entry point for pipeline execution.
+     *
+     * @param args Command line arguments to the pipeline.
+     */
+    public static void main(String[] args) {
+        CdapHubspotSourceOptions options =
+                PipelineOptionsFactory.fromArgs(args).withValidation().as(CdapHubspotSourceOptions.class);
+
+        // Create the pipeline
+        Pipeline pipeline = Pipeline.create(options);
+        run(pipeline, options);
+    }
+
+    public static PipelineResult run(Pipeline pipeline, CdapHubspotSourceOptions options) {
+        Map<String, Object> paramsMap = PluginConfigOptionsConverter.hubspotOptionsToParamsMap(options);
+        LOG.info("Starting Cdap-Hubspot-to-txt pipeline with parameters: {}", paramsMap);
+
+        /*
+         * Steps:
+         *  1) Read messages in from Cdap Hubspot
+         *  2) Extract values only
+         *  3) Write successful records to PubSub
+         */
+        pipeline.getCoderRegistry().registerCoderForClass(JsonElement.class, JsonElementCoder.of());
+
+        pipeline
+                .apply("readFromCdapHubspot", FormatInputTransform.readFromCdapHubspot(paramsMap))
+                .setCoder(
+                        KvCoder.of(
+                                NullableCoder.of(WritableCoder.of(NullWritable.class)), JsonElementCoder.of()))
+                .apply(
+                        MapValues.into(TypeDescriptors.strings())
+                                .via(
+                                        jsonElement -> {
+                                            if (jsonElement == null) {
+                                                return "{}";
+                                            }
+                                            return jsonElement.toString();
+                                        }))
+                .setCoder(
+                        KvCoder.of(
+                                NullableCoder.of(WritableCoder.of(NullWritable.class)), StringUtf8Coder.of()))
+                .apply(Values.create())
+                .apply("writeToPubSub", PubsubIO.writeStrings().to(options.getOutputTopic()));
+
+        return pipeline.run();
+    }
+}
